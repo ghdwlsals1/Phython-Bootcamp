cd94f6c9acab08290d96d59e17e8fe9dca5edb9e
Jinmin <ghdwlsals1@gmail.com>
2026-07-30 00:16:46 +0900

    Day3: Python if, else, elif , and ,or

---
+++ Day3/if_01.py
@@ -0,0 +1,9 @@
+score=75
+if score >= 80:
+    print("합격")
+else:
+    print("불합격")
+print("시험 종료")
+
+
+
---
+++ Day3/if_02.py
@@ -0,0 +1,8 @@
+money=300
+age=14
+if money >= 10000 or age >= 20:
+    print("PC방 입장")
+elif money >= 5000 or age >= 15:
+    print("코인 노래방 입장")
+else:
+    print("집에 간다")
