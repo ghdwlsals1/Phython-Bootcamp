bfa1e5f934acaa71b85dca2b022cc22afd61aef0
Jinmin <ghdwlsals1@gmail.com>
2026-07-31 00:14:53 +0900

    practice while loop

---
+++ Day3/mini_project01.py
@@ -0,0 +1,10 @@
+money=5000
+age=20
+if money >=5000 and age >=20:
+    print("맥주를 산다")
+elif money >=2000:
+    print("콜라를 산다")
+elif money >=1000:
+    print("물을 산다")
+else:
+    print("집에 간다")
---
+++ Day4 While/x=1.py
@@ -0,0 +1,5 @@
+x=5
+while x <= 1:
+    print(x)
+    x=(x-1)
+print("종료")
