1a7940d5bcf1d5a2dd72246f4ef78a2054b5bc76
Jinmin <ghdwlsals1@gmail.com>
2026-08-01 01:39:22 +0900

    Day5 메뉴프로그램 완성

---
+++ Day5 while+input/Menu01.py
@@ -0,0 +1,16 @@
+print("==========메뉴===========")
+print("1. 게임 시작")
+print("2. 설정")
+print("3. 종료")
+print("=========================")
+while True:
+    print("메뉴")
+    menu=int(input("번호를 입력해주세요:"))
+    if menu==1:
+        print("게임시작")
+    elif menu ==2:
+        print("설정")
+    elif menu ==3:
+        print("종료")
+        break
+print("프로그램 종료")
