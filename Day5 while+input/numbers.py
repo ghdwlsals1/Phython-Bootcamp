422e09041d5c8368af53f4c5375810a1d7bc0cfd
Jinmin <ghdwlsals1@gmail.com>
2026-09-02 13:28:14 +0900

    items

---
+++ Day5 while+input/numbers.py
@@ -0,0 +1,6 @@
+items=["방패","칼","포션","검"]
+for item in items:
+    if len(item) ==2:
+        print(item)
+
+
--- Day5 while+input/while Ture 테스트.py
+++ Day5 while+input/while Ture 테스트.py
@@ -4,11 +4,22 @@ while True:
     answer=int(input("숫자를 입력해주세요."))
     if answer==0:
         break
+    if answer < 0:
+        print("음수는 건너뜁니다.")
+        continue
+    if answer < 10:
+        print("10보다 작습니다.")
+        continue
+
+
     if answer % 2==0:
-        print("짝수입니다:",answer)
+        print("짝수입니다.:",answer)
         even_numbers.append(answer)
     else:
-        print("홀수입니다:",answer)
+        print("홀수입니다.:",answer)
         odd_numbers.append(answer)
-print(f"짝수",even_numbers)
-print(f"홀수",odd_numbers)
+print("짝수:",even_numbers)
+print("홀수:",odd_numbers)
+
+
+
